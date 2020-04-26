ee2f9089047b170804f5bd038df2def9232ecf31
Dasip <dasipos@gmail.com>
2020-04-26 19:09:27 +0300

    make it better

---
+++ Bot.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import Updater
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+
+TOKEN = "1203054967:AAE6HsWJxFcL8FoNWLlv9_gxPuHcjrdxe6Y"
+TG_API = "https://telegg.ru/orig/bot"
+
+
+def message_handler(update: Update, context):
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=update.effective_message.text)
+
+
+def main():
+    my_update = Updater(
+        token=TOKEN,
+        use_context=True,
+        base_url=TG_API)
+
+    m_handler = MessageHandler(Filters.all, message_handler)
+    my_update.dispatcher.add_handler(m_handler)
+
+    my_update.start_polling(timeout=2)
+    my_update.idle()
+
+
+if __name__ == "__main__":
+    main()
